6aee2b5e9149d41396e33e9aff5d2a32bc8a1212
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-05-27 13:13:41 +0100

    Container Shared Data

---
+++ src/containers/SharedData/SharedData.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import styles from "./SharedData.module.css";
+
+const SharedData = (props) => {
+  return <div className={styles.SharedData}>Movies and photos</div>;
+};
+
+export default SharedData;
