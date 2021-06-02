e7b9666d2566a5c282dc6e83907cc5e7007981b7
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-06-02 17:53:34 +0100

    downloade data shered data

--- src/containers/SharedData/SharedData.js
+++ src/containers/SharedData/SharedData.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "../../components/Carousel/Carousel";
 import styles from "./SharedData.module.css";
+import axios from "../../axios-data";
+import imagesWithMetadata from "../../metadata.json";
 
-const importAllimages = (r) => r.keys().map((item) => r(item).default);
+// const importAllimages = (r) => r.keys().map((item) => r(item).default);
+const importAllimages = (r) =>
+  r.keys().map((item) => {
+    const imageUrl = r(item).default;
+    const image = imagesWithMetadata.find((imgMeta) =>
+      imageUrl.includes(imgMeta.id)
+    );
+    return { ...image, imageUrl };
+  });
 
 const images = importAllimages(
   require.context("../../assets/images", false, /\.(png|jpg|svg)$/)
@@ -10,9 +20,23 @@ const images = importAllimages(
 
 const SharedData = (props) => {
   const [active, setActive] = useState(0);
+
+  // useEffect(() => {
+  //   axios.put("marathon-2366475").then((res) => {
+  //     console.log(res.data);
+  //   });
+  // }, []);
+  console.log(imagesWithMetadata);
+
   return (
     <div className={styles.SharedData}>
       <Carousel items={images} active={active} onSetActive={setActive} />
+      {/* <Carousel
+        items={images}
+        active={active}
+        metadata={imagesWithMetadata}
+        onSetActive={setActive}
+      /> */}
     </div>
   );
 };
