dcb04f49a338346febaf5e9f493cc514a4028f75
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-06-03 14:41:54 +0100

    imghist demo

--- logic/imagehistogramsdemo.js
+++ logic/imagehistogramsdemo.js
@@ -1,26 +1,32 @@
 const path = require("path");
-const PHOTO_PATH = path.join(__dirname, "./images/baseball-1536097_1920.jpg");
+// const PHOTO_PATH = path.join(__dirname, "./images/baseball-1536097.jpg");
+const PHOTO_PATH = path.join(
+  __dirname,
+  "../src/assets/images/marathon-2366475.jpg"
+);
 // const PHOTO_PATH = path.join(__dirname, "./images/bananas-1119790_1920.jpg");
 
 function createHistograms() {
   const Jimp = require("jimp");
-  const imghist = require("./imagehistograms.js");
+  const Hist = require("./imagehistograms.js");
 
   Jimp.read(PHOTO_PATH, function (err, photo) {
     if (err) {
       console.error(err);
     } else {
-      const histred = imghist.histogramRGB(imghist.colorChannels.Red, photo);
-      saveHistogram(histred, "histred.svg");
+      const histRed = Hist.histogramRGB(Hist.colorChannels.Red, photo);
+      const histGreen = Hist.histogramRGB(Hist.colorChannels.Green, photo);
+      const histBlue = Hist.histogramRGB(Hist.colorChannels.Blue, photo);
+      const histY = Hist.histogramYCbCr(Hist.colorChannels.Y, photo);
+      const histCb = Hist.histogramYCbCr(Hist.colorChannels.Cb, photo);
+      const histCr = Hist.histogramYCbCr(Hist.colorChannels.Cr, photo);
 
-      const histgreen = imghist.histogramRGB(
-        imghist.colorChannels.Green,
-        photo
-      );
-      saveHistogram(histgreen, "histgreen.svg");
-
-      let histblue = imghist.histogramRGB(imghist.colorChannels.Blue, photo);
-      saveHistogram(histblue, "histblue.svg");
+      saveHistogram(histRed, "../src/assets/histograms/Histred.svg");
+      saveHistogram(histGreen, "../src/assets/histograms/Histgreen.svg");
+      saveHistogram(histBlue, "../src/assets/histograms/Histblue.svg");
+      saveHistogram(histY, "../src/assets/histograms/HistY.svg");
+      saveHistogram(histCb, "../src/assets/histograms/HistCb.svg");
+      saveHistogram(histCr, "../src/assets/histograms/HistCr.svg");
     }
   });
 }
