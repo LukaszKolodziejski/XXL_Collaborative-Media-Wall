b116cfb3ddf8e87d008edb725b517473f2d07ae3
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-06-03 14:42:44 +0100

    better carusel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,21 +3,22 @@ import { CSSTransitionGroup } from "react-transition-group/";
 import CarouselItem from "./CarouselItem/CarouselItem";
 import "./styles/Carousel.css";
 import Modal from "../UI/Modal/Modal";
+import Metadata from "../Metadata/Metadata";
+import Button from "../UI/Button/Button";
 
 const Carousel = (props) => {
   const [isModal, setIsModal] = useState(false);
   const [activeID, setActiveID] = useState({});
+  const [showMetadata, setShowMetadata] = useState(false);
 
   const generateItems = () => {
     const items = [];
     let level;
+    const { length } = props.items;
     for (let i = props.active - 2; i < props.active + 3; i++) {
       let index = i;
-      if (i < 0) {
-        index = props.items.length + i;
-      } else if (i >= props.items.length) {
-        index = i % props.items.length;
-      }
+      index = i < 0 ? length + i : i >= length ? i % length : i;
+
       level = props.active - i;
       items.push(
         <CarouselItem
@@ -33,19 +34,24 @@ const Carousel = (props) => {
   };
 
   const modalHandler = () => setIsModal((prev) => !prev);
-  const activeIdHandler = (id) => {
-    console.log(props.items.find((item) => item.id === id));
+  const activeIdHandler = (id) =>
     setActiveID(props.items.find((item) => item.id === id));
-  };
+  const btnHandler = () => setShowMetadata((prev) => !prev);
+
   return (
     <>
       <Modal show={isModal} modalClosed={modalHandler}>
+        <Button clicked={btnHandler}>{`${
+          showMetadata ? "Hide" : "Show"
+        } Metadata`}</Button>
         <CarouselItem
           level={9}
           metadata={activeID}
           activeMmetadata={activeIdHandler}
           modalClosed={modalHandler}
-        />
+        >
+          <Metadata show={showMetadata} metadata={activeID} />
+        </CarouselItem>
       </Modal>
       <div id="carousel" className="noselect">
         <div
