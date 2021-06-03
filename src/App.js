4dd78f7ff0757ba0bc546a2120bee8fe2234bd1d
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-06-03 21:01:53 +0100

    dynamic App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,36 @@ import Layout from "./hoc/Layout/Layout";
 import SharedData from "./containers/SharedData/SharedData";
 // import Auth from "./containers/Auth/Auth";
 // import Logout from "./containers/Auth/Logout/Logout";
+import imagesWithMetadata from "./metadata.json";
+
+const importAllimages = (r) =>
+  r.keys().map((item) => {
+    const imageUrl = r(item).default;
+    const image = imagesWithMetadata.find((imgMeta) =>
+      imageUrl.includes(imgMeta.id)
+    );
+    return { ...image, imageUrl };
+  });
+
+const images = importAllimages(
+  require.context("./assets/images", false, /\.(png|jpg|svg)$/)
+);
 
 const App = () => {
-  const users = ["Łukasz", "Elsie", "Willie", "Sheri"];
+  const users = ["shared", "Łukasz", "Elsie", "Willie", "Sheri"];
 
   return (
     <Router>
       <Layout>
-        <Route path="/shared" exact component={SharedData} />
+        {users.map((user) => (
+          <Route
+            key={user}
+            path={`/folder-${user}`}
+            component={() => (
+              <SharedData images={images} user={user} users={users} />
+            )}
+          />
+        ))}
       </Layout>
     </Router>
   );
