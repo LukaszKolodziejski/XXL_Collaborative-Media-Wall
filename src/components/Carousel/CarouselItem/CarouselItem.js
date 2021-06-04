30665699be41b277289ddba51885929cedf4fcda
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-06-04 02:45:47 +0100

    full drag photo

--- src/components/Carousel/CarouselItem/CarouselItem.js
+++ src/components/Carousel/CarouselItem/CarouselItem.js
@@ -1,27 +1,64 @@
 import React, { useState } from "react";
-import { useSpring, animated } from "@react-spring/web";
-import { useDrag } from "react-use-gesture";
 import "./styles/CarouselItem.css";
+import { useSpring, animated, to } from "@react-spring/web";
+import { useGesture } from "react-use-gesture";
+
+const calcX = (y, ly) => -(y - ly - window.innerHeight / 2) / 20;
+const calcY = (x, lx) => (x - lx - window.innerWidth / 2) / 20;
 
 const CarouselItem = (props) => {
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
-
-  // Set the drag hook and define component movement based on gesture data
-  const bind = useDrag(({ down, movement: [mx, my] }) => {
-    api.start({ x: down ? mx : 0, y: down ? my : 0 });
-  });
   const doubleClickHandler = () => {
     props.modalClosed();
     props.activeMmetadata(props.metadata.id);
   };
 
+  const [{ x, y, rotateX, rotateY, rotateZ, zoom, scale }, set] = useSpring(
+    () => ({
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+      scale: 1,
+      zoom: 0,
+      x: 0,
+      y: 0,
+      config: { mass: 2, tension: 50, friction: 20 },
+    })
+  );
+
+  const [drag, setDrag] = useState(false);
+
+  const bind = useGesture({
+    onDragStart: () => setDrag(true),
+    onDrag: ({ offset: [x, y] }) =>
+      set({ x, y, rotateX: 0, rotateY: 0, scale: 1 }),
+    onDragEnd: () => setDrag(false),
+    onMove: ({ xy: [px, py], dragging }) =>
+      !dragging &&
+      set({
+        rotateX: calcX(py, y.get()),
+        rotateY: calcY(px, x.get()),
+        scale: 1.05,
+      }),
+    onPinch: ({ offset: [d, a] }) => set({ zoom: d / 20, rotateZ: a * 20 }),
+    onHover: ({ hovering }) =>
+      !hovering && set({ rotateX: 0, rotateY: 0, scale: 1 }),
+  });
+
   const bindOptions = props.isAnimate ? { ...bind() } : null;
   return (
     <animated.div
       {...bindOptions}
-      style={{ x, y }}
       className={`item level${props.level}`}
       onDoubleClick={doubleClickHandler}
+      style={{
+        transform: "perspective(600px)",
+        x,
+        y,
+        scale: to([scale, zoom], (s, z) => s + z),
+        rotateX,
+        rotateY,
+        rotateZ,
+      }}
     >
       <div
         className="image"
