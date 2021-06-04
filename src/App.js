deea5ee8f987cbb537bf5798d37c085381162cef
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-06-04 12:04:49 +0100

    fetch image to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import SharedData from "./containers/SharedData/SharedData";
@@ -6,21 +6,26 @@ import SharedData from "./containers/SharedData/SharedData";
 // import Logout from "./containers/Auth/Logout/Logout";
 import imagesWithMetadata from "./metadata.json";
 
-const importAllimages = (r) =>
-  r.keys().map((item) => {
-    const imageUrl = r(item).default;
-    const image = imagesWithMetadata.find((imgMeta) =>
-      imageUrl.includes(imgMeta.id)
-    );
-    return { ...image, imageUrl };
-  });
-
-const images = importAllimages(
-  require.context("./assets/images", false, /\.(png|jpg|svg)$/)
-);
-
 const App = () => {
+  const importAllimages = (r) =>
+    r.keys().map((item) => {
+      const imageUrl = r(item).default;
+      const image = imagesWithMetadata.find((imgMeta) =>
+        imageUrl.includes(imgMeta.id)
+      );
+      return { ...image, imageUrl };
+    });
+
+  const fetchImages = importAllimages(
+    require.context("./assets/images", false, /\.(png|jpg|svg)$/)
+  );
+
+  const [images, setImages] = useState(fetchImages);
   const users = ["shared", "Łukasz", "Elsie", "Willie", "Sheri"];
+
+  useEffect(() => {
+    setImages(fetchImages);
+  }, [imagesWithMetadata]);
 
   return (
     <Router>
