3bad9990056cfc2b08de45f0646dec3ab4328e60
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-06-04 01:49:01 +0100

    useDrag  react use gesture

--- src/components/Carousel/CarouselItem/CarouselItem.js
+++ src/components/Carousel/CarouselItem/CarouselItem.js
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
+import { useDrag } from "react-use-gesture";
 import "./styles/CarouselItem.css";
 
 const CarouselItem = (props) => {
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
+
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    api.start({ x: down ? mx : 0, y: down ? my : 0 });
+  });
   const doubleClickHandler = () => {
     props.modalClosed();
     props.activeMmetadata(props.metadata.id);
   };
 
+  const bindOptions = props.isAnimate ? { ...bind() } : null;
   return (
-    <div
+    <animated.div
+      {...bindOptions}
+      style={{ x, y }}
       className={`item level${props.level}`}
       onDoubleClick={doubleClickHandler}
     >
-      <img
+      <div
         className="image"
-        src={props.metadata.imageUrl}
-        alt={props.metadata.tags}
+        style={{ backgroundImage: `url(${props.metadata.imageUrl})` }}
       />
       {props.children}
-    </div>
+    </animated.div>
   );
 };
 
